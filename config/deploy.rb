280388ef9e01e166e63eb6b9e6d30dd477c09c0e
Msjoycullen <xiaojizhang27@gmail.com>
2016-09-17 16:31:36 -0400

    Fix capistrano deploy issue

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,9 @@ set :deploy_to, '/home/looprac/looprac-api'
 set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+set :bundle_binstubs, nil
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
