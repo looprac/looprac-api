1fcb5d41b28e6a72a33f5b74da0287999da51c7f
Msjoycullen <xiaojizhang27@gmail.com>
2016-09-18 05:20:27 -0400

    Add create trip

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -6,9 +6,10 @@ class TripsController < ApplicationController
   end
 
   def create
-    @trip = Trip.create(trip_params.merge(user_id: User.find_by_email(params[:email])))
+    @trip = Trip.create(trip_params.merge(user_id: User.find_by_email(params[:email]).id))
     if @trip.save
-      Net::HTTP.post_form(URI.parse("http://localhost:3869"), @trip.to_json)
+      Net::HTTP.post_form(URI.parse("http://localhost:3869/trip/create"), 
+                          @trip.to_json(except: [:origin_name, :destin_name, :user_id], methods: :driver_uuid))
     else
       render json: { error: "cannot create trip" }
     end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,5 @@
-require 'net/http'
-
 class Trip < ActiveRecord::Base
+  def driver_uuid
+    User.find(user_id).uuid
+  end
 end
